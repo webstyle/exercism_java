9ac7956258474880becc08be29b7c86d44502be5
Farrukh <webstyle@protonmail.com>
2017-08-07 13:03:00 +0500

    raindrops

--- raindrops/src/main/java/RaindropConverter.java
+++ raindrops/src/main/java/RaindropConverter.java
@@ -1,7 +1,55 @@
 class RaindropConverter {
+    private String result;
+
+    RaindropConverter() {
+        this.result = "";
+    }
 
     String convert(int number) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+
+        if (number % 3 == 0) {
+            this.append("Pling");
+        } else if (number % 5 == 0) {
+            this.append("Plang");
+        } else if (number % 7 == 0) {
+            this.append("Plong");
+        } else {
+            this.append(Integer.toString(number));
+        }
+
+        this.checkMultiple(number);
+
+        return this.getResult();
+    }
+
+    private String getResult() {
+        return this.result;
+    }
+
+    private void append(String input) {
+        this.result += input;
+    }
+
+    private void reassign(String input) {
+        this.result = input;
+    }
+
+    private void checkMultiple(int number) {
+        if (number % 3 == 0 && number % 5 == 0) {
+            this.reassign("PlingPlang");
+        }
+
+        if (number % 3 == 0 && number % 7 == 0) {
+            this.reassign("PlingPlong");
+        }
+
+        if (number % 5 == 0 && number % 7 == 0) {
+            this.reassign("PlangPlong");
+        }
+
+        if (number % 3 == 0 && number % 5 == 0 && number % 7 == 0) {
+            this.reassign("PlingPlangPlong");
+        }
     }
 
 }
